7df155526bcc5a2390e1859afcdf9803a51f2f86
Renisha Christie. A <christie.reni@gmail.com>
2018-11-03 17:14:35 +0530

    Customize global theme

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import * as serviceWorker from './serviceWorker';
 require('dotenv').config();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#646464',
+      main: '#3e3e3e',
+      dark: '#2b2b2b',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#999999',
+      main: '#808080',
+      dark: '#595959',
+      contrastText: '#fff',
+    },
+  },
+});
+
+ReactDOM.render(<MuiThemeProvider theme={theme}><App /></MuiThemeProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
