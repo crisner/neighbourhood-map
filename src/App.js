4bdaa77b49c786c4fd74b078404e310835e5759b
Renisha Christie. A <christie.reni@gmail.com>
2018-11-03 17:14:11 +0530

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import NavBar from './components/NavBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <NavBar />
+        {/* <Sidebar />
+        <Map />
+        <Footer /> */}
       </div>
     );
   }
