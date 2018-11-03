2438f2712cb3d787e7e3ee3b4ef74d12e3c7217e
Renisha Christie. A <christie.reni@gmail.com>
2018-11-03 12:51:54 +0530

    Require dotenv config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+require('dotenv').config();
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
