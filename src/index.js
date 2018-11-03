ea33640cd0282186c41bca6c0c7bb5bcfa540daa
Renisha Christie. A <christie.reni@gmail.com>
2018-11-03 14:32:05 +0530

    Reorder require config to the end

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-require('dotenv').config();
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
