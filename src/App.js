fa0b436b5a44d497138022ba2a538d897b217ef2
Renisha Christie. A <christie.reni@gmail.com>
2018-11-05 13:48:42 +0530

    Add sample data

--- src/App.js
+++ src/App.js
@@ -6,11 +6,32 @@ import Footer from './components/Footer';
 import './App.css';
 
 class App extends Component {
+
+    state = {
+        location_details: [
+            {
+            "res_id": 3000048,
+            "latitude": "10.9941971417",
+            "longitude": "76.9633718580"
+            },
+            {
+            "res_id": 3000195,
+            "latitude": "11.0017780550",
+            "longitude": "76.9760167971",
+            },
+            {
+            "res_id": 3001572,
+            "latitude": "11.0137631621",
+            "longitude": "76.9468960539"
+            }
+        ]
+    }
+
   render() {
     return (
       <div className="App">
         <NavBar />
-        <Map />
+        <Map location={this.state.location_details} />
         <Footer />
       </div>
     );
