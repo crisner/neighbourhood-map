6d5827e5b20f12cd59b46c2cfdd7821ba3380a5f
Renisha Christie. A <christie.reni@gmail.com>
2018-11-05 20:51:02 +0530

    Add methods to get restaurant data from api and get all location data

--- src/App.js
+++ src/App.js
@@ -8,24 +8,56 @@ import './App.css';
 class App extends Component {
 
     state = {
-        location_details: [
-            {
-            "res_id": 3000048,
-            "latitude": "10.9941971417",
-            "longitude": "76.9633718580"
-            },
-            {
-            "res_id": 3000195,
-            "latitude": "11.0017780550",
-            "longitude": "76.9760167971",
-            },
-            {
-            "res_id": 3001572,
-            "latitude": "11.0137631621",
-            "longitude": "76.9468960539"
-            }
-        ]
+        location_details: [],
+        restaurant_details: [],
+        restaurant_reviews: []
     }
+
+getAllLocations = () => {
+    const Url = 'https://developers.zomato.com/api/v2.1/search?entity_id=30&entity_type=city&lat=11.0168&lon=76.9558&establishment_type=16&category=1%2C2%2C5&sort=rating&order=desc&start=0';
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        "user-key": process.env.REACT_APP_ZOMATO_ACCESS_TOKEN
+    });
+    return fetch(Url, {headers}).then(res => res.json());
+}
+
+getRestasurantDetails = (resId) => {
+    const Url = `https://developers.zomato.com/api/v2.1/restaurant?res_id=${resId}`;
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        "user-key": process.env.REACT_APP_ZOMATO_ACCESS_TOKEN
+    });
+    return fetch(Url, {headers}).then(res => res.json());
+}
+
+getReviews = (resId) => {
+    const Url = `https://developers.zomato.com/api/v2.1/reviews?res_id=${resId}`;
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        "user-key": process.env.REACT_APP_ZOMATO_ACCESS_TOKEN
+    });
+    return fetch(Url, {headers}).then(res => res.json());
+}
+
+componentDidMount() {
+    this.getAllLocations()
+    .then(data => {
+        this.setState(() => ({
+            location_details: data.restaurants.map(info => {
+                return {
+                    res_id: info.restaurant.R.res_id,
+                    name: info.restaurant.name,
+                    latitude: info.restaurant.location.latitude,
+                    longitude: info.restaurant.location.longitude
+                }
+            })
+        }))
+        console.log(this.state);
+    })
+    .catch(error => console.log(error))
+}
+
 
   render() {
     return (
