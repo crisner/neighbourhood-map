b088cecf1cdf541ba902d44f962000c91ef61935
Renisha Christie. A <christie.reni@gmail.com>
2018-11-06 16:03:56 +0530

    Populate selected restaurant information in popup

--- src/containers/Map.js
+++ src/containers/Map.js
@@ -28,11 +28,32 @@ class Map extends Component {
       longitude: 76.961632,
       zoom: 11
     },
-    popupInfo: null
+    popupInfo: null,
+    restaurant_details: [],
+    restaurant_reviews: []
   };
 
+  getRestasurantDetails = (resId) => {
+    const Url = `https://developers.zomato.com/api/v2.1/restaurant?res_id=${resId}`;
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        "user-key": process.env.REACT_APP_ZOMATO_ACCESS_TOKEN
+    });
+    return fetch(Url, {headers}).then(res => res.json());
+  }
+
+  getReviews = (resId) => {
+      const Url = `https://developers.zomato.com/api/v2.1/reviews?res_id=${resId}`;
+      const headers = new Headers({
+          "Content-Type": "application/json",
+          "user-key": process.env.REACT_APP_ZOMATO_ACCESS_TOKEN
+      });
+      return fetch(Url, {headers}).then(res => res.json());
+  }
+
   showPopup = () => {
-    const {popupInfo} = this.state;
+    const {popupInfo, restaurant_details, restaurant_reviews} = this.state;
+    const {location, user_rating} = this.state.restaurant_details;
 
     return popupInfo && (
         <Popup
@@ -41,9 +62,35 @@ class Map extends Component {
         closeButton={true}
         onClose={() => this.setState({popupInfo: null})}
         anchor="bottom">
-          <div>{popupInfo.name}</div>
+          <h3>{popupInfo.name}</h3>
+          {restaurant_details.thumb === "" ? null : (
+            <img src={restaurant_details.thumb} alt={`Picture of ${restaurant_details.name}`} width="100px" />
+          )}
+          <p>{user_rating === undefined ? null : user_rating.aggregate_rating}
+          <span>{user_rating === undefined ? null : (user_rating.votes === "1" ? ` 1 vote` : ` ${user_rating.votes} votes`)}</span></p>
+          <p>{location === undefined ? null : location.address}</p>
+          <h5>Reviews</h5>
+          <ul>
+            {restaurant_reviews.user_reviews === undefined ? null : (
+              restaurant_reviews.user_reviews.map((review, index) => index < 2 ? (
+              <li key={`${restaurant_details.id}${index}`}>{review.review.review_text}</li>) : null)
+            )}
+          </ul>
         </Popup>
     )
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(this.state.popupInfo !== prevState.popupInfo && this.state.popupInfo !== null) {
+      const resId = this.state.popupInfo.res_id;
+      this.getRestasurantDetails(resId)
+      .then(details => {
+        this.setState({restaurant_details: details})
+        this.getReviews(resId)
+        .then(reviews => this.setState({restaurant_reviews: reviews}))
+        .catch(error => console.log(error, 'Cannot find restaurant reviews'))
+      }).catch(error => console.log(error, 'Cannot find restaurant details'))
+    }
   }
 
   render() {
@@ -65,11 +112,12 @@ class Map extends Component {
             longitude={Number(coords.longitude)}
             offsetLeft={-20} offsetTop={-10}>
               <RestaurantIcon className={classes.icon}
-              onClick={() => this.setState({popupInfo: coords})}/>
+              onClick={() => {this.setState({popupInfo: coords})}}/>
             </Marker>
           )
         })}
         {this.showPopup()}
+        {console.log(this.state)}
       </ReactMapGL>
     );
   }
