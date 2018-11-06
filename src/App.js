a7064effb8ac1b6a597bfdba812c82109160062c
Renisha Christie. A <christie.reni@gmail.com>
2018-11-06 16:05:16 +0530

    Move state and methods for restaurant details to map container

--- src/App.js
+++ src/App.js
@@ -8,31 +8,11 @@ import './App.css';
 class App extends Component {
 
     state = {
-        location_details: [],
-        restaurant_details: [],
-        restaurant_reviews: []
+        location_details: []
     }
 
     getAllLocations = () => {
         const Url = 'https://developers.zomato.com/api/v2.1/search?entity_id=30&entity_type=city&lat=11.0168&lon=76.9558&establishment_type=16&category=1%2C2%2C5&sort=rating&order=desc&start=0';
-        const headers = new Headers({
-            "Content-Type": "application/json",
-            "user-key": process.env.REACT_APP_ZOMATO_ACCESS_TOKEN
-        });
-        return fetch(Url, {headers}).then(res => res.json());
-    }
-
-    getRestasurantDetails = (resId) => {
-        const Url = `https://developers.zomato.com/api/v2.1/restaurant?res_id=${resId}`;
-        const headers = new Headers({
-            "Content-Type": "application/json",
-            "user-key": process.env.REACT_APP_ZOMATO_ACCESS_TOKEN
-        });
-        return fetch(Url, {headers}).then(res => res.json());
-    }
-
-    getReviews = (resId) => {
-        const Url = `https://developers.zomato.com/api/v2.1/reviews?res_id=${resId}`;
         const headers = new Headers({
             "Content-Type": "application/json",
             "user-key": process.env.REACT_APP_ZOMATO_ACCESS_TOKEN
@@ -57,7 +37,6 @@ class App extends Component {
         })
         .catch(error => console.log(error))
     }
-
 
     render() {
         return (
