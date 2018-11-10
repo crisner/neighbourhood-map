5a47f45b95a60a95fd62c6d754dfbe81c76260d7
Renisha Christie. A <christie.reni@gmail.com>
2018-11-10 20:10:04 +0530

    Add custom droppin

---
+++ src/components/ChipPin.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+
+const ChipPin = (color = null) => {
+  const styles = theme => {
+    const backgroundColor = color || theme.palette.background.default;
+
+    return {
+      root: {
+        color: theme.palette.getContrastText(backgroundColor),
+        backgroundColor,
+      },
+      clickable: {
+        cursor: 'pointer',
+        '&:hover, &:focus': {
+          backgroundColor: '#f77848',
+        },
+        '&:active': {
+          backgroundColor: '#f77848',
+        },
+      },
+    };
+  };
+
+  const CustomChip = ({ classes, ...props }) =>
+    <Chip classes={classes} {...props} />;
+
+  return withStyles(styles)(CustomChip);
+};
+
+export default ChipPin;
