639f501f6e032c8d3da36af5c66318f31f489194
crisner <christie.reni@gmail.com>
2018-12-21 16:29:24 +0530

    Updates

---
+++ precache-manifest.7d3f9eb84738766a36714de142e89abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4f46611e166d396c4e4",
+    "url": "/neighbourhood-map/static/js/runtime~main.b4f46611.js"
+  },
+  {
+    "revision": "4d212bc2f7ca31408d63",
+    "url": "/neighbourhood-map/static/js/main.4d212bc2.chunk.js"
+  },
+  {
+    "revision": "c7fcbbcda7124d4583f0",
+    "url": "/neighbourhood-map/static/js/1.c7fcbbcd.chunk.js"
+  },
+  {
+    "revision": "4d212bc2f7ca31408d63",
+    "url": "/neighbourhood-map/static/css/main.b820619c.chunk.css"
+  },
+  {
+    "revision": "c7fcbbcda7124d4583f0",
+    "url": "/neighbourhood-map/static/css/1.9021c072.chunk.css"
+  },
+  {
+    "revision": "7359283674b1844d8c578a6afae01081",
+    "url": "/neighbourhood-map/index.html"
+  }
+];
